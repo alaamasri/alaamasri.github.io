7ca51ee7d23831cd84ab9766e2d578c115c99036
alaamasri <alaaip20@gmail.com>
2016-03-07 03:37:19 +0200

    test
    test

---
+++ Portfolio_files/Tatweer/Scripts/scripts.js
@@ -0,0 +1,60 @@
+$(function () {
+
+    /*Preload Mobile onHover*/
+    $.preloadImages = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            $("<img />").attr("src", arguments[i]);
+        }
+    }
+
+    $.preloadImages("./Images/loadingLogo.png");
+
+    /* Slider touch */
+    $("#slider li.active").next().click();
+    $("#slider").swiperight(function () {
+        $(this).carousel('prev');
+    });
+
+    $("#slider").swipeleft(function () {
+        $(this).carousel('next');
+    });
+
+    $('.carousel').carousel({
+        interval: 5000
+    });
+
+    $('.carousel').carousel('cycle');
+
+    $('.carousel').on('slide.bs.carousel', function (e) {
+        $('.carousel .left-caption.animated').addClass('fadeInLeft');
+        $('.carousel .right-caption.animated').addClass('fadeInRight');
+    });
+
+
+}); /*== Document ready== */
+
+$(window).load(function () {
+
+    new WOW({ offset: 100 }).init();
+    if ($(window).width() > 767) {
+        var captionTop = $('.menu-container').height() + 40;
+        $('#slider .caption').css({ "top": captionTop });
+        $('#slider .item').height($('#slider .item .large-slide')[0].height);
+    }
+  
+    $('.loading').fadeOut('slow');
+    $('html').removeClass('locked');
+    //$('#slider').animo('blur');
+    //$('#slider .left-caption').animo({ animation: 'fadeInLeft', duration: 1 });
+
+});
+
+$(window).resize(function () {
+    if ($(window).width() > 767) {
+        var captionTop = $('.menu-container').height() + 40;
+        $('#slider .caption').css({ "top": captionTop });
+
+        $('#slider .item').height($('#slider .item .large-slide')[0].height);
+    }
+   
+});
