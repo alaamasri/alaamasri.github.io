206a38aa99cd3277e75f5fcb28a7cc7f3740292b
alaamasri <alaaip20@gmail.com>
2017-10-08 15:46:14 +0300

    Updates

---
+++ Pages/Nova/js/contactengine.php
@@ -0,0 +1,93 @@
+<?php
+$Name = Trim(stripslashes($_POST['name'])); 
+$Email = Trim(stripslashes($_POST['email'])); 
+$Subject = Trim(stripslashes($_POST['subject'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
+
+
+if (empty($Name) || empty($Email)){
+	
+}
+    
+else
+{
+$Body = "";
+$Body .= "<strong>Name:</strong> ";
+$Body .= $Name;
+$Body .= "<br><br>";
+$Body .= "<strong>Email:</strong> ";
+$Body .= $Email;
+$Body .= "<br><br>";
+$Body .= "<strong>Subject:</strong> ";
+$Body .= $Subject;
+$Body .= "<br><br>";
+$Body .= "<strong>Message:</strong><br><br> ";
+$Body .= $Message;
+$Body .= "<br><br>";
+
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+
+require 'PHPMailerAutoload.php';
+
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 0;
+
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "alawtadwebsite@gmail.com";
+
+//Password to use for SMTP authentication
+$mail->Password = "nova12345678";
+
+//Set who the message is to be sent from
+
+$mail->SetFrom($Email, $Name);
+
+//Set an alternative reply-to address
+$mail->addReplyTo($Email, $Name);
+
+//Set who the message is to be sent to
+ 
+ $mail->addAddress('info@alawtad-co.com', 'Alawatad Contact Form');
+ //$mail->addAddress('taffconstructions@gmail.com', 'Taff Contracting Establishment');
+$mail->Subject = 'Alawatad Website Contact Us Message';
+$mail->IsHTML(true);
+$mail->Body= $Body;
+$mail->CharSet = "text/html; charset=UTF-8;";
+$mail->AltBody = $Body;
+
+
+
+
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
+}
