f25e58df26041354525f5ab31bf06398dabae267
Alaa Almasri <alaaip20@gmail.com>
2020-01-22 06:34:33 +0300

    js

---
+++ Pages/GlobalFlavours/js/inner.js
@@ -0,0 +1,50 @@
+  /*------------------------------------------------------------------------------*/
+    /* Menu
+    /*------------------------------------------------------------------------------*/
+
+
+$(function () {
+    $(".header-container").load('./index.html #masthead',function(){
+        $(".mega-menu-item.active").removeClass("active");
+        $(".main-menu a").each(function () {
+            if ($(this).attr("href").indexOf("#")>-1){
+                if($(this).attr("href").length<2){
+                    $(this).attr("href","/")
+                }
+                else{
+                    $(this).attr("href","./"+$(this).attr("href"))
+                }
+            }
+            
+            if ($(this).attr("href").replace("./", "") == window.location.pathname.split("/").pop()) {
+                $(this).parent().addClass("active");
+                $(this).parents(".mega-menu-item").addClass("active");
+                $(this).attr("href","#");
+            }
+            
+        });
+        
+    
+
+    $('.btn-show-menu-mobile').on('click', function () {
+        $(this).toggleClass('is-active');
+        $('.menu-mobile').slideToggle();
+    });
+    
+    });
+   
+
+    $(".footer-container").load('./index.html #mastfooter')
+
+    $(".rp-container").load('./index.html #homeProducts', function () {
+        $(".rp-container .featured-thumbnail a").each(function () {
+            if ($(this).attr("href").replace("./", "") == window.location.pathname.split("/").pop()) {
+                $(this).parents(".ttm-box-col-wrapper.col-lg-4").remove();
+            }
+        });
+        $(".slick_slider").slick();
+
+    })
+
+
+})
